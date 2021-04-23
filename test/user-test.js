fa1133d28a0c1277e34e9067c790175bbe0a9947
Nichele Dunn <the7yearitch@hotmail.com>
2021-04-23 17:34:56 -0600

    ✨ Set up skeleton for user test

--- test/user-test.js
+++ test/user-test.js
@@ -1,1 +1,15 @@
-import { expect } from 'chai';
+import { expect } from "chai";
+
+import User from "../src/User.js";
+import { userData } from "../src/test/test-data.js";
+
+describe("The User class", () => {
+
+
+
+
+
+
+
+
+});
