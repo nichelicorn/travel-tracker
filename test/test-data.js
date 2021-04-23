22d6d97300ab0cafca28b970c6ccb6f0f9e6676b
Nichele Dunn <the7yearitch@hotmail.com>
2021-04-23 16:25:31 -0600

    ✨ Add test data

--- test/test-data.js
+++ test/test-data.js
@@ -1,11 +1,33 @@
 const userData = [
-  {}
-]
+  {"id":1,"name":"River Song"},
+  {"id":3,"name":"Clara Oswald"},
+  {"id":7,"name":"Rose Tyler",},
+  {"id":13,"name":"Bill Potts"},
+  {"id":"agent","name":"The Doctor"}
+];
 
 const travelerData = [
-  {}
-]
+  {"id":1,"name":"River Song","travelerType":"history buff"},
+  {"id":3,"name":"Clara Oswald","travelerType":"thrill-seeker"},
+  {"id":7,"name":"Rose Tyler","travelerType":"thrill-seeker"},
+  {"id":13,"name":"Bill Potts","travelerType":"relaxer"}
+];
 
 const agentData = [
-  {}
-]
+  {"id":"agent","name":"The Doctor"}
+];
+
+const trips = [
+  {"id":1,"userID":1,"destinationID":49,"travelers":1,"date":"2019/09/16","duration":8,"status":"approved","suggestedActivities":[]},
+  {"id":2,"userID":3,"destinationID":25,"travelers":5,"date":"2020/10/04","duration":18,"status":"pending","suggestedActivities":[]},
+  {"id":3,"userID":7,"destinationID":22,"travelers":4,"date":"2020/05/22","duration":17,"status":"pending","suggestedActivities":[]},
+  {"id":4,"userID":13,"destinationID":14,"travelers":2,"date":"2020/02/25","duration":10,"status":"approved","suggestedActivities":[]},
+  {"id":5,"userID":1,"destinationID":29,"travelers":3,"date":"2020/04/30","duration":18,"status":"approved","suggestedActivities":[]},
+  {"id":6,"userID":3,"destinationID":35,"travelers":3,"date":"2020/06/29","duration":9,"status":"pending","suggestedActivities":[]},
+  {"id":7,"userID":7,"destinationID":17,"travelers":5,"date":"2020/5/28","duration":20,"status":"approved","suggestedActivities":[]},
+  {"id":8,"userID":13,"destinationID":39,"travelers":6,"date":"2021/02/07","duration":4,"status":"approved","suggestedActivities":[]},
+  {"id":9,"userID":1,"destinationID":19,"travelers":5,"date":"2019/12/19","duration":19,"status":"pending","suggestedActivities":[]},
+  {"id":10,"userID":3,"destinationID":50,"travelers":6,"date":"2020/07/23","duration":17,"status":"approved","suggestedActivities":[]},
+  {"id":11,"userID":7,"destinationID":5,"travelers":4,"date":"2020/10/14","duration":4,"status":"approved","suggestedActivities":[]},
+  {"id":12,"userID":13,"destinationID":33,"travelers":6,"date":"2020/10/17","duration":6,"status":"pending","suggestedActivities":[]},
+];
