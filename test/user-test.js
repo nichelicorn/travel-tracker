5efe6e0c74dfd236afceef3fed4d5e73dc7f2e64
Nichele Dunn <the7yearitch@hotmail.com>
2021-04-23 17:37:22 -0600

    📝 Add pseudo to user-test

--- test/user-test.js
+++ test/user-test.js
@@ -5,11 +5,18 @@ import { userData } from "../src/test/test-data.js";
 
 describe("The User class", () => {
 
-
-
-
-
-
-
-
 });
+
+// it should be an instance of a User
+
+// it should have an id
+  // id can be a number or string
+
+// it should have a name
+  // name should be a string
+
+// it should have a userName
+  // userName should be a string
+
+// it should have a password
+  // password should be a string
