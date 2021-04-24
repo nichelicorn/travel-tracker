61fe76d5405ffffe11d223b87e0315573ea7849c
Nichele Dunn <the7yearitch@hotmail.com>
2021-04-24 13:24:04 -0600

    ♻️  Refactor user and traveler data

--- test/test-data.js
+++ test/test-data.js
@@ -1,26 +1,74 @@
-const userData = [
-  {"id":1,"name":"River Song","userName":"traveler1","password":"travel2020"},
-  {"id":3,"name":"Clara Oswald","userName":"","password":"travel2020"},
-  {"id":7,"name":"Rose Tyler","userName":"","password":"travel2020"},
-  {"id":13,"name":"Bill Potts","userName":"","password":"travel2020"},
-  {"id":"agent","name":"The Doctor","userName":"","password":""}
-];
+const userData = {
+  "users": [
+    {"id":1,
+    "name":"River Song",
+    "userName":"traveler1",
+    "password":"travel2020"},
+    {"id":3,
+    "name":"Clara Oswald",
+    "userName":"traveler3",
+    "password":"travel2020"},
+    {"id":7,
+    "name":"Rose Tyler",
+    "userName":"traveler7",
+    "password":"travel2020"},
+    {"id":13,
+    "name":"Bill Potts",
+    "userName":"traveler13",
+    "password":"travel2020"},
+    {"id":"agent"
+    ,"name":"The Doctor",
+    "userName":"agent",
+    "password":"travel2020"}
+  ]
+}
 
-const travelerData = [
-  {"id":1,"name":"River Song","travelerType":"history buff"},
-  {"id":3,"name":"Clara Oswald","travelerType":"thrill-seeker"},
-  {"id":7,"name":"Rose Tyler","travelerType":"thrill-seeker"},
-  {"id":13,"name":"Bill Potts","travelerType":"relaxer"}
-];
+const travelerData = {
+  "travelers":[
+    {"id":1,
+    "name":"River Song",
+    "travelerType":"history buff"},
+    {"id":3,
+    "name":"Clara Oswald",
+    "travelerType":"thrill-seeker"},
+    {"id":7,
+    "name":"Rose Tyler",
+    "travelerType":"thrill-seeker"},
+    {"id":13,
+    "name":"Bill Potts",
+    "travelerType":"relaxer"}
+  ]
+};
 
 const agentData = [
   {"id":"agent","name":"The Doctor"}
 ];
 
 const trips = [
-  {"id":1,"userID":1,"destinationID":1,"travelers":1,"date":"2019/09/16","duration":8,"status":"approved","suggestedActivities":[]},
-  {"id":2,"userID":3,"destinationID":2,"travelers":5,"date":"2020/10/04","duration":18,"status":"pending","suggestedActivities":[]},
-  {"id":3,"userID":7,"destinationID":3,"travelers":4,"date":"2020/05/22","duration":17,"status":"pending","suggestedActivities":[]},
+  {"id":1,
+  "userID":1,
+  "destinationID":1,
+  "travelers":1,
+  "date":"2019/09/16",
+  "duration":8,
+  "status":"approved",
+  "suggestedActivities":[]},
+  {"id":2,
+  "userID":3,
+  "destinationID":2,
+  "travelers":5,
+  "date":"2020/10/04",
+  "duration":18,
+  "status":"pending",
+  "suggestedActivities":[]},
+  {"id":3,
+  "userID":7,
+  "destinationID":3,
+  "travelers":4,
+  "date":"2020/05/22",
+  "duration":17,
+  "status":"pending",
+  "suggestedActivities":[]},
   {"id":4,"userID":13,"destinationID":4,"travelers":2,"date":"2020/02/25","duration":10,"status":"approved","suggestedActivities":[]},
   {"id":5,"userID":1,"destinationID":5,"travelers":3,"date":"2020/04/30","duration":18,"status":"approved","suggestedActivities":[]},
   {"id":6,"userID":3,"destinationID":6,"travelers":3,"date":"2020/06/29","duration":9,"status":"pending","suggestedActivities":[]},
