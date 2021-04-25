68ade79b89c697e0712049f262faaf8d042e3dc2
Nichele Dunn <the7yearitch@hotmail.com>
2021-04-25 13:22:19 -0600

    ✨ Add files for Trip and trip-test

---
+++ test/traveler-test.js
@@ -0,0 +1,30 @@
+import { expect } from "chai";
+
+import Traveler from "../src/Traveler.js";
+import { travelerData } from "../test/test-data.js";
+
+let traveler1, traveler3, agent;
+
+describe("The Traveler class", () => {
+
+
+
+})
+
+// it should inherit the User class properties/methods
+
+// test outcomes of inherited properties and methods
+
+// TRAVELER PROPERTIES
+// it should have a property travelerType
+// it should have a property trips [] - this will store a user's trips
+// it should have a property amt spent 0
+
+// it should have a method to request a new trip (instantiate a new Trip)
+  // must be able to select:
+    // date
+    // duration
+    // # of travelers
+    // destination
+    // est total cost
+    // 10% agent fee
