1a92750159d759a0781f1654c5172a617b56b816
Nichele Dunn <the7yearitch@hotmail.com>
2021-04-25 13:29:13 -0600

    ✨ Add hook to traveler-test

--- test/traveler-test.js
+++ test/traveler-test.js
@@ -3,11 +3,18 @@ import { expect } from "chai";
 import Traveler from "../src/Traveler.js";
 import { travelerData } from "../test/test-data.js";
 
-let traveler1, traveler3, agent;
+let traveler1, traveler3, traveler7, traveler13;
 
 describe("The Traveler class", () => {
 
+  beforeEach(() => {
+    traveler1 = new Traveler(travelerData[0]);
+    traveler3 = new Traveler(travelerData[1]);
+    traveler7 = new Traveler(travelerData[2]);
+    traveler13 = newTraveler(travelerData[3]);
+  })
 
+    
 
 })
 
