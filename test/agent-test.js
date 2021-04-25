696037d581315933fcf30383c4410d8bcf871386
Nichele Dunn <the7yearitch@hotmail.com>
2021-04-25 14:41:47 -0600

    ✨ Set up Agent skeleton and initial test block

---
+++ test/agent-test.js
@@ -0,0 +1,16 @@
+import { expect } from "chai";
+
+import Agent from "../src/Agent.js";
+import { agentData } from "../test/test-data.js";
+import { travelerData } from "../test/test-data.js"; // this seems like it may be redundant - can agentData and travelerData be imported in one line?
+
+let agent, traveler1;
+
+describe("The Agent class", () => {
+
+  beforeEach(() => {
+    agent = new Agent(agentData[0]);
+    traveler1 = new Traveler(travelerData[0]);
+  })
+
+})
